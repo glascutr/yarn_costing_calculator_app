982e8539e2432b7bcea528230a1616d4b703856a
mn-48 <nazmul.cse48@gmail.com>
2026-02-08 10:42:07 +0600

    android App

---
+++ android/app/src/main/kotlin/com/example/yarn_costing_calculator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.yarn_costing_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {
+            print('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            print('Page finished loading: $url');
+          },
+          onWebResourceError: (WebResourceError error) {
+            print('Web resource error: ${error.description}');
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://teamex.glascutr.com'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Yarn Costing Calculator')),
+        body: WebViewWidget(controller: controller),
+      ),
+    );
+  }
+}
